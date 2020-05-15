93edd117b920ac98720e770f6d5267eed4ccfed6
oomaroomar <omar.mayani@gmail.com>
2020-05-15 23:49:25 +0300

    Update docker.sh
    fixed docker to work with 20.04 hopefully

--- software/docker.sh
+++ software/docker.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-sudo apt update
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
